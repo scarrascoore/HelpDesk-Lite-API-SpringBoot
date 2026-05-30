57a5c452b73f03f6d7aa27cd14ab82bc7de0c8a3
scarrascoore <scarrascoore@gmail.com>
2026-05-29 19:17:46 -0500

    feat: Implementar gestion de HelpDesk con Spring Boot

---
+++ src/main/java/com/portfolio/helpdesk/HelpdeskLiteApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskLiteApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskLiteApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/helpdesk/HelpdeskLiteApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskLiteApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
